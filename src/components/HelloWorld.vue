0b426195154b504f2232b4aea64d417eb428fc7b
ShionKoga <shion_koga@mail.toyota.co.jp>
2021-11-15 14:38:41 +0900

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,11 +31,18 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  mounted() {
+    axios.get("/api").then((res) => {
+      console.log(res.data);
+      this.apiMsg = res.data;
+    });
+  },
 }
 </script>
 
